e846f409bda5f1221003ca5fe32040da2ca951b3
MathGabr <50830710+MathGabr@users.noreply.github.com>
2019-10-01 14:15:12 -0300

    Add files via upload

---
+++ 2_AreaTriangulo.cpp
@@ -0,0 +1,24 @@
+/*Função: calcula a área de um triangulo
+Autor: Matheus Gabriel e Silva
+Data:30/09/2019
+*/
+#include<stdio.h>
+#include<windows.h>
+#include<locale.h>
+
+int main (){
+	setlocale(LC_ALL, "");
+	float b = 0.0, h = 0.0, area = 0.0;
+	
+	printf("Informe a base do triângulo: ");
+	scanf("%f",&b);
+
+	printf("Informe a altura do triângulo: ");
+	scanf("%f",&h);
+	
+	area = b*h/2;
+	
+	printf("A área do triângulo é: %.2f\n\n\n",area);
+	system("pause");
+		
+}
