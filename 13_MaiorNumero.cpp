bd44a20c7b869a643b7f0a07e2d438d5621242fe
MathGabr <50830710+MathGabr@users.noreply.github.com>
2019-10-16 10:08:21 -0300

    Update 13_MaiorNumero.cpp

--- 13_MaiorNumero.cpp
+++ 13_MaiorNumero.cpp
@@ -1,6 +1,7 @@
-/*Função: recebe um valor inteiro e se for maior que 5 e menor que 20, exibe seu cubo
+/*FunÃ§Ã£o: recebe trÃªs valores e exibe o maior nÃºmero entre eles
 Autor: Matheus Gabriel e Silva
 Data:09/10/2019
+Data alteraÃ§Ã£o:16/10/2019
 */
 #include<stdio.h>
 #include<windows.h>
@@ -10,13 +11,13 @@ int main(){
 	setlocale(LC_ALL, "");
 	float a = 0, b=0, c=0, maior = 0, cont = 0; 
 	
-	printf("Informe um número: ");
+	printf("Informe um nÃºmero: ");
 	scanf("%f", &a);
 	
-	printf("Informe outro número: ");
+	printf("Informe outro nÃºmero: ");
 	scanf("%f", &b);
 	
-	printf("Informe mais um número: ");
+	printf("Informe mais um nÃºmero: ");
 	scanf("%f", &c);
 	
 	maior = a;
@@ -31,7 +32,7 @@ int main(){
 		cont = cont + 1;
 	}
 	
-	printf("\nO maior número é: %.2f \n\n\n", maior);
+	printf("\nO maior nÃºmero Ã©: %.2f \n\n\n", maior);
 
 	system("pause");	
 }
